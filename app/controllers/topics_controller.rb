0b52f109d79f1aae1f3f9574238da0c8799e5cc6
takuya nagasawa <tsukasayanagaw474@gmail.com>
2021-06-13 18:02:57 +0900

    topic機能作成

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,10 @@
+class TopicsController < ApplicationController
+  def new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'topics/new'
+  get 'topics/show'
+  get 'topics/edit'
+  get 'users/new'
+  get 'users/show'
+  get 'users/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
