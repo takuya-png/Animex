b77ce36fd2b20380b6b8179b2d54873ab3b5e9be
takuya nagasawa <tsukasayanagaw474@gmail.com>
2021-06-13 18:18:35 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-  get 'topics/new'
-  get 'topics/show'
-  get 'topics/edit'
-  get 'users/new'
-  get 'users/show'
-  get 'users/edit'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
