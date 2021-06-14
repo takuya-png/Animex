632a1c4ad98c7cb9fb83dcb4ee7daf03ec584468
takuya nagasawa <tsukasayanagaw474@gmail.com>
2021-06-14 13:23:01 +0900

    ルーティング追記

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   resources :topics
   root 'topics#index'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
