dcae4834ad788a76e53ebd625960be912e76ccff
takuya nagasawa <tsukasayanagaw474@gmail.com>
2021-06-15 16:34:56 +0900

    ヘッダー作成

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,7 +5,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   storage :file
-  process :resize_to_limit => [50, 50]
+  process :resize_to_limit => [2000, 2000]
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
