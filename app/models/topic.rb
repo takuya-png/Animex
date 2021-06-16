de65e12a43f99afd71424b181083e304db3ef736
takuya nagasawa <tsukasayanagaw474@gmail.com>
2021-06-16 22:56:25 +0900

    commentコントローラ作成

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,6 +1,7 @@
 class Topic < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
+  has_many :comments, dependent: :destroy
   belongs_to :user
   validates :title, presence: true, length: {maximum: 50}
   validates :content, presence: true, length: {maximum: 300}
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'comments/create'
   resources :topics do
     resources :comments
   end
