6f0604177e859f5a48891672b29bb13936b5123d
takuya nagasawa <tsukasayanagaw474@gmail.com>
2021-06-16 22:42:39 +0900

    commentモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_16_083027) do
+ActiveRecord::Schema.define(version: 2021_06_16_134130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "topic_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["topic_id"], name: "index_comments_on_topic_id"
+  end
 
   create_table "favorites", force: :cascade do |t|
     t.bigint "user_id"
@@ -57,6 +65,7 @@ ActiveRecord::Schema.define(version: 2021_06_16_083027) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "topics"
   add_foreign_key "favorites", "topics"
   add_foreign_key "favorites", "users"
 end
