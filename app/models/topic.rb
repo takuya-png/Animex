0ebbb8601a78ac3891250b31f5a75940a3d94bcd
takuya nagasawa <tsukasayanagaw474@gmail.com>
2021-06-16 15:04:09 +0900

    アソシエーション記述

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,6 @@
 class Topic < ApplicationRecord
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_users, through: :favorites, source: :user
   validates :title, presence: true, length: {maximum: 50}
   validates :content, presence: true, length: {maximum: 300}
   mount_uploader :image, ImageUploader
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_13_130721) do
+ActiveRecord::Schema.define(version: 2021_06_16_054713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "topic_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["topic_id"], name: "index_favorites_on_topic_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "topics", force: :cascade do |t|
     t.string "title"
@@ -46,4 +55,6 @@ ActiveRecord::Schema.define(version: 2021_06_13_130721) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "favorites", "topics"
+  add_foreign_key "favorites", "users"
 end
