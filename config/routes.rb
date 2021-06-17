0466349cae2a2f92ddf72031130c02a80a18af2b
takuya nagasawa <tsukasayanagaw474@gmail.com>
2021-06-17 22:45:41 +0900

    ルーティング追記

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :topics
   resources :favorites, only: [:create, :destroy]
   resources :users, only: [:index]
+  resources :relationships, only: [:create, :destroy]
   root 'topics#index'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: {
