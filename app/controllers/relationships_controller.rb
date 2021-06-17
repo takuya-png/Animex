a779c55d8d0dc944ee8831b3cdf7e10cb45a3f25
takuya nagasawa <tsukasayanagaw474@gmail.com>
2021-06-17 23:22:35 +0900

    フォローのcreateメソッドの処理を実装する

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,5 +1,11 @@
 class RelationshipsController < ApplicationController
+  before_action :authenticate_user!
+  respond_to? :js
   def create
+    if logged_in?
+      @user = User.find(params[:relationship][:followed_id])
+      current_user.follow!(@user)
+    end
   end
 
   def destroy
