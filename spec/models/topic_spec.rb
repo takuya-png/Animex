dc35aaf2603c20a98ad4d5b7dcd644ed49fcbe78
takuya nagasawa <tsukasayanagaw474@gmail.com>
2021-06-24 23:04:34 +0900

    topicモデルテスト作成

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -1,7 +1,5 @@
 require 'rails_helper'
 describe 'topicモデル機能', type: :model do
-  # let!(:user) {create(:user)}
-  # let!(:topic) {create(:topic, user_id: user.id)}
   before do
     @user = FactoryBot.create(:user)
   end
@@ -9,7 +7,11 @@ describe 'topicモデル機能', type: :model do
   describe 'バリデーションテスト' do
     context 'タイトルの入力が正しい場合' do
       it '新規作成ができる' do
-        expect(topic).to be_valid
+        topic = Topic.new(title: 'aaa',
+                          content: 'aaa',
+                          image: Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/icon10.jpg')),
+                          user_id: @user.id)
+        expect(topic).not_to be_invalid
       end
     end
     context 'タイトルが空の場合' do
@@ -24,27 +26,36 @@ describe 'topicモデル機能', type: :model do
     context 'タイトルが51文字以上の場合' do
       it 'バリデーションエラーとなる' do
         topic = Topic.new(title: 'a'*51,
-        content: 'aaa',
-        image: Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/icon10.jpg')),
-        user_id: @user.id)
-
+                          content: 'aaa',
+                          image: Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/icon10.jpg')),
+                          user_id: @user.id)
         expect(topic).to be_invalid
       end
     end
     context 'コンテントの入力が正しい場合' do
       it '新規作成できる' do
+        topic = Topic.new(title: 'a',
+                          content: 'aaa',
+                          image: Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/icon10.jpg')),
+                          user_id: @user.id)
         expect(topic).to be_valid
       end
     end
     context 'コンテントが空の場合' do
       it 'バリデーションエラーとなる' do
-        topic.content= nil
+        topic = Topic.new(title: 'aaa',
+                          content: '',
+                          image: Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/icon10.jpg')),
+                          user_id: @user.id)
         expect(topic).to be_invalid
       end
     end
     context 'コンテントが301文字以上の場合' do
       it 'バリデーションエラーとなる' do
-        topic.content = 'a'*301
+        topic = Topic.new(title: 'aaa',
+                          content: 'a'*301,
+                          image: Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/icon10.jpg')),
+                          user_id: @user.id)
         expect(topic).to be_invalid
       end
     end
