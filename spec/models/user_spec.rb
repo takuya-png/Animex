890b281aefd7d1fa39682f3dec3c4ac6d4f76512
takuya nagasawa <tsukasayanagaw474@gmail.com>
2021-06-24 23:47:51 +0900

    userモデルスペック作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,21 +10,6 @@ RSpec.describe User, type: :model do
       expect(@user).to be_valid
     end
 
-    it 'ユーザー名が空の場合NG' do
-      @user.name = nil
-      expect(@user.valid?).to eq(false)
-    end
-
-    it 'ユーザー名が15文字以上の場合NG' do
-      @user.name = 'a'*16
-      expect(@user.valid?).to eq(false)
-    end
-
-    it 'メールアドレスが空の場合NG' do
-      @user.email = nil
-      expect(@user.valid?).to eq(false)
-    end
-
     it 'メールアドレスが規定外の場合NG' do
       @user.email =  'gmail.com@'
       expect(@user.valid?).to eq(false)
