d852463cd047647f892653ec02e9bba58eeec0a5
takuya nagasawa <tsukasayanagaw474@gmail.com>
2021-06-25 17:33:16 +0900

    set topic system spec

--- spec/system/topic_spec.rb
+++ spec/system/topic_spec.rb
@@ -7,37 +7,38 @@ RSpec.describe '投稿管理機能', type: :system do
   # let!(:favorite){create(:favorite,topic_id: topic3.id, user_id: user.id)}
 
   before do
-    visit root_path
-    # binding.irb
-    sleep(0.5)
-    click_link 'ログイン'
+    visit new_user_session_path
+    sleep(0.1)
     fill_in 'user[email]',with: 'takuya@docomo.ne.jp'
     fill_in 'user[password]',with: 'password'
-    sleep(0.5)
     click_button 'ログイン'
-    sleep(0.5)
+    sleep(0.1)
     click_link 'Topic一覧'
+    sleep(0.1)
   end
 
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
-        # binding.irb
+        sleep(0.1)
         click_link '新規投稿'
+        sleep(0.1)
         expect(current_path).to have_content '/topics/new'
         fill_in 'タイトル',with: '確認用タイトル'
         fill_in 'コンテンツ', with: '確認用コンテント'
-        sleep(0.5)
         click_button '投稿'
+        sleep(0.1)
         expect(page).to have_content '確認用タイトル'
         expect(page).to have_content '確認用コンテント'
-        sleep(0.5)
       end
     end
     context 'タスク作成に失敗した場合' do
       it '画面遷移せずそのまま新規投稿画面にいること' do
+        sleep(0.1)
         click_link '新規投稿'
+        sleep(0.1)
         click_button 'commit'
+        sleep(0.1)
         expect(current_path).to have_content '/topics'
       end
     end
@@ -45,21 +46,22 @@ RSpec.describe '投稿管理機能', type: :system do
   describe 'お気に入り登録機能' do
     context 'お気に入りした場合' do
       it 'お気に入り一覧に表示される' do
-        # binding.irb
         first('tr:nth-child(4) td:nth-child(7)').click
         # find('#favorite').click
         click_on 'お気に入り一覧'
+        sleep(0.1)
         expect(current_path).to have_content "/favorites"
         expect(page).to have_content 'お気に入り一覧'
       end
     end
     context 'お気に入り解除した場合' do
       it 'お気に入り一覧に表示されなくなる' do
-        # binding.irb
+        sleep(0.1)
         first('tr:nth-child(4) td:nth-child(7)').click
         expect(page).to have_content '詳細'
         first('tr:nth-child(4) td:nth-child(7)').click
         click_on 'お気に入り一覧'
+        sleep(0.1)
         expect(current_path).to have_content '/favorites'
         expect(page).not_to have_content '魔王学院の不適合者'
       end
@@ -68,40 +70,38 @@ RSpec.describe '投稿管理機能', type: :system do
   describe '投稿編集機能' do
     context '自分の投稿を編集する場合' do
       it '投稿が編集される' do
-        # binding.irb
-        first('tr:nth-child(3) td:nth-child(5)').click
-        # find(".sid_content").click
+        find_by_id("topics-index_row-#{topic.id}_edit").click
+        title = 'タイトル'
+        content = 'コンテンツ'
+        fill_in title, with: '確認用タイトル'
+        fill_in content, with: '確認用コンテント'
         click_on '更新'
-        expect(page).to have_content "タイトル"
-        # fill_in "topic[タイトル]"
-        # first('tr:nth-child(3) td:nth-child(4)').click
-        # expect(page).to have_content "投稿詳細"
-        # expect(page).to have_content "編集しましたよ"
-        # sleep(0.5)
+        sleep(0.1)
+        expect(find_by_id("topics-index_row-#{topic.id}_title")).to have_content title
       end
     end
   end
   describe '投稿削除機能' do
     context '投稿を削除する場合' do
       it '投稿が削除できる' do
-        # binding.irb
-        first('tr:nth-child(2) td:nth-child(6)').click
+        topic = Topic.last
+        find_by_id("topics-index_row-#{topic.id}_destroy").click
         page.driver.browser.switch_to.alert.accept
-        # click_on 'OK'
-        # click_on '投稿削除'
-        # page.driver.browser.switch_to.alert.accept
-        # click_on 'Myプロフィール'
-        expect(page).not_to have_content '魔王学院の不適合者'
+        expect(page).not_to have_selector("#topics-index_row-#{topic.id}")
       end
     end
   end
   describe 'トピック検索機能' do
     context 'トピック検索した場合' do
       it '該当の投稿が表示される' do
-        fill_in "q[title_or_content_cont]" ,with: '東京'
+        sleep(0.1)
+        fill_in "q[title_or_content_cont]" ,with: topic3.title
         click_button '検索'
-        expect(page).to have_content '検索結果'
-        expect(page).to have_content '熊本県天草市'
+        sleep(0.1)
+        lists = all(".topics-index_row")
+        node = find_by_id("topics-index_row-#{topic3.id}")
+        expect(lists.length).to eq 1
+        expect(node).to be_present
       end
     end
   end
